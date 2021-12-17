04584f05e523027c4cbaabbffc3a8008394c5c4a
Tais Massaro <code@tais.dev>
2021-12-17 19:10:48 +0100

    feat(libs): add functionality to topic button

--- libs/shared/ui/src/lib/topic-button/topic-button.stories.tsx
+++ libs/shared/ui/src/lib/topic-button/topic-button.stories.tsx
@@ -1,20 +1,27 @@
 import { Story, Meta } from '@storybook/react';
+import { useState } from 'react';
 import { TopicButton, TopicButtonProps } from './topic-button';
 
 export default {
   component: TopicButton,
   title: 'TopicButton',
+  argTypes: {
+    onClick: { action: 'clicked' },
+  },
 } as Meta;
 
 const Template: Story<TopicButtonProps> = (args) => {
+  const [topic, setTopic] = useState<string | null>(null);
+
   return (
     <div className="bg-indigo-100 p-20">
-      <TopicButton {...args} />
+      <TopicButton {...args} onClick={(topic: string) => setTopic(topic)} />
+      {topic && <p className="mt-2">{topic} button has been clicked!</p>}
     </div>
   );
 };
 
 export const Primary = Template.bind({});
 Primary.args = {
-  name: 'Primary',
+  name: 'Next.js',
 };
--- libs/shared/ui/src/lib/topic-button/topic-button.tsx
+++ libs/shared/ui/src/lib/topic-button/topic-button.tsx
@@ -1,16 +1,40 @@
+import { useEffect, useState } from 'react';
 import './topic-button.module.css';
 
 export interface TopicButtonProps {
   name: string;
+  onClick?: (name: string) => void;
 }
 
-export function TopicButton({ name }: TopicButtonProps) {
+export function TopicButton({ name, onClick }: TopicButtonProps) {
+  const [icon, setIcon] = useState<string>('');
+
+  useEffect(() => {
+    async function getIcon() {
+      const svgFilename = name.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+      const Icon = await import(`./${svgFilename}.svg`);
+      setIcon(Icon.default);
+    }
+
+    getIcon();
+  }, [name]);
+
+  function onClickHandler() {
+    if (onClick) {
+      onClick(name);
+    } else {
+      console.warn(`No onClick handler for ${name} topic button`);
+    }
+  }
+
   return (
-    <div className="bg-white pl-4 rounded-lg shadow flex max-w-md min-w-max hover:shadow-md transition-shadow">
-      <div className="p-5">
-        <h1 className="font-bold text-4xl">{name}</h1>
-      </div>
-    </div>
+    <button
+      className="bg-white pl-4 rounded-lg shadow flex items-center max-w-md min-w-max hover:shadow-md transition-shadow"
+      onClick={onClickHandler}
+    >
+      <img src={icon} alt={`${name} icon`} className="w-12" />
+      <h1 className="font-bold text-4xl p-4">{name}</h1>
+    </button>
   );
 }
 
