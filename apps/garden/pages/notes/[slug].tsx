49a0c20f10f20cc1bf244d33ac62320676af8cbf
Tais Massaro <code@tais.dev>
2021-12-17 15:46:33 +0100

    feat(libs): add markdown support package

--- apps/garden/pages/notes/[slug].tsx
+++ apps/garden/pages/notes/[slug].tsx
@@ -1,5 +1,12 @@
+import { readdirSync } from 'fs';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { join } from 'path';
 import { ParsedUrlQuery } from 'querystring';
+import { MDXRemote } from 'next-mdx-remote';
+import {
+  getParsedFileContentBySlug,
+  renderMarkdown,
+} from '@egghead-digital-garden/markdown';
 import './[slug].module.css';
 
 /* eslint-disable-next-line */
@@ -7,10 +14,18 @@ export interface NoteProps extends ParsedUrlQuery {
   slug: string;
 }
 
-export function Note(props: NoteProps) {
+const NOTES_PATH = join(process.cwd(), '_notes');
+
+export function Note({ frontmatter, html }) {
+  console.log('🚀 ~ file: [slug].tsx ~ line 20 ~ Note ~ html', html);
   return (
-    <div>
-      <h1>Visiting {props.slug}</h1>
+    <div className="m-6">
+      <article className="prose prose-lg">
+        <h1>{frontmatter.title}</h1>
+        <div>by {frontmatter.author.name}</div>
+      </article>
+      <hr />
+      <MDXRemote {...html} />
     </div>
   );
 }
@@ -20,16 +35,30 @@ export const getStaticProps: GetStaticProps<NoteProps> = async ({
 }: {
   params: NoteProps;
 }) => {
+  // 1. parse markdown content into frontmatter and content
+  const { frontmatter, content } = getParsedFileContentBySlug(
+    params.slug,
+    NOTES_PATH
+  );
+
+  // 2. convert content to html
+  const htmlContent = await renderMarkdown(content);
+
   return {
     props: {
-      slug: params.slug,
+      frontmatter,
+      html: htmlContent,
     },
   };
 };
 
 export const getStaticPaths: GetStaticPaths<NoteProps> = async (context) => {
+  const paths = readdirSync(NOTES_PATH)
+    .map((file) => file.replace(/\.mdx?$/, ''))
+    .map((slug: string) => ({ params: { slug } }));
+
   return {
-    paths: [{ params: { slug: 'page1' } }, { params: { slug: 'page2' } }],
+    paths,
     fallback: false,
   };
 };
---
+++ libs/markdown/src/lib/markdown.ts
@@ -0,0 +1,20 @@
+import { readFileSync } from 'fs';
+import * as matter from 'gray-matter';
+import { join } from 'path';
+import { serialize } from 'next-mdx-remote/serialize';
+
+export function getParsedFileContentBySlug(filename: string, path: string) {
+  const filePath = join(path, `${filename}.mdx`);
+  const fileContent = readFileSync(filePath, 'utf8');
+
+  const { data, content } = matter(fileContent);
+
+  return {
+    frontmatter: data,
+    content,
+  };
+}
+
+export function renderMarkdown(content: string) {
+  return serialize(content || '');
+}
